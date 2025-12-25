31d415b773f397f2677e7a4093280179e237ee8d
ayeshkant <ayeshkant.mishra7@gmail.com>
2025-12-25 12:02:04 +0530

    claims implementation

---
+++ ePizzaHub.UI/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ePizzaHub.UI.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ePizzaHub.UI/Utils/BasePageView.cs
@@ -0,0 +1,30 @@
+﻿using ePizzaHub.UI.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Security.Claims;
+
+namespace ePizzaHub.UI.Utils
+{
+    public abstract class BasePageView<TModel>:RazorPage<TModel>
+    {
+        public UserViewModel CurrentUser
+        {
+            get
+            {
+                if (User.Claims.Any())
+                {
+                    string userName = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value.ToString();
+                    string email = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value.ToString();
+                    string userId = User.Claims.FirstOrDefault(x => x.Type == "UserId").Value.ToString();
+
+                    return new UserViewModel
+                    {
+                        Email = email,
+                        Name = userName,
+                        UserId = Convert.ToInt32(userId)
+                    };
+                }
+                return null;
+            }
+        }
+    }
+}
